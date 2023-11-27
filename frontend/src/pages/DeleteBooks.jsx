6f11de9a63ef9bdd7538c8b875c8565774a17f86
kapil <upadhyaykapil61@gmail.com>
2023-11-27 20:18:15 +0530

    UI for home page and for its components

--- frontend/src/pages/DeleteBooks.jsx
+++ frontend/src/pages/DeleteBooks.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React  , {useState} from 'react'
+import BackButton from '../components/BackButton'
+import Spinner from '../components/Spinner'
+import axios from 'axios'
+import { useNavigate , useParams } from 'react-router-dom'
+
 
 export default function DeleteBooks() {
+  const [loading , setLoading] = useState(false);
+  const Navigate = useNavigate();
+  const {id} = useParams();
+  const handleDeleteBook = () =>{
+    setLoading(true);
+    axios.delete(`http://localhost:5400/books/${id}`).then(()=>{
+      setLoading(false);
+      Navigate("/")
+    }).catch((error)=>{
+      setLoading(false);
+      alert("an error occured");
+      console.log(error);
+    })
+  }
   return (
-    <div>
-      deleting it
+    <div className='p-4'>
+      <BackButton/>
+      <h1 className='text-3xl my-4'>Delete Book</h1>
+      {loading ? <Spinner/>: ""}
+      <div className='flex flex-col items-center border-2 border-sky-400 rounded-xl w-[6000px] p-8 mx-auto'></div>
+      <h3 className='text-2xl'>are you want to delete this book ?</h3>
+      <button className='p-4 bg-red-600 text-white m-8 w-full' onClick={handleDeleteBook}>Yes,Delete..</button>
     </div>
   )
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,7 +5,9 @@ import Spinner from "../components/Spinner";
 import {Link} from "react-router-dom";
 import {AiOutlineEdit} from "react-icons/ai"
 import {BsInfoCircle} from "react-icons/bs"
-import {MdOutlineAddBox , MdOutlineDelete} from "react-icons/md"
+import {MdOutlineAddBox , MdOutlineDelete} from "react-icons/md";
+import BooksTable from "../components/home/BooksTable";
+import BooksCard from  "../components/home/BooksCard";
 
 export default function Home() {
     const [books , setBooks] = useState([]);
@@ -31,53 +33,10 @@ export default function Home() {
            <MdOutlineAddBox className="text-sky-800 text-4xl" />
          </Link>
         </div>
-        {loading ? (
+        {loading ? 
           <Spinner/>
-        ):(
-        <table className="w-full border-separate border-spacing-2">
-          <thead>
-            <tr>
-              <th className="border border-slate-600 rounded-md">No</th>
-              <th className="border border-slate-600 rounded-md">Title</th>
-              <th className="border border-slate-600 rounded-md max-md:hidden">Author</th>
-              <th className="border border-slate-600 rounded-md max-md:hidden">Publish year</th>
-              <th className="border border-slate-600 rounded-md ">Operations</th>
-
-              
-            </tr>
-          </thead>
-          <tbody>
-            {books.map((book,index)=>{
-              <tr key={book._id} className="h-8">
-                <td className="border border-slate-700 rounded-md text-center">
-                  {index+1}
-                </td>
-                <td className="border border-slate-700 rounded-md text-center">
-                  {book.title}
-                </td>
-                <td className="border border-slate-700 rounded-md text-center max-md:hidden">
-                  {book.author}
-                </td>
-                <td className="border border-slate-700 rounded-md text-center max-md:hidden">
-                  {book.publishYear}
-                </td>
-                <td className="border border-slate-700 rounded-md text-center">
-                  <div className="flex justify-center gap-x-4">
-                    <Link to = { `/books/details/${book._id}`}>
-                      <BsInfoCircle className="text-2xl text-green-800"/> 
-                    </Link>
-                    <Link to = { `/books/edit/${book._id}`}>
-                      <AiOutlineEdit className="text-2xl text-yellow-600"/> 
-                    </Link>
-                    <Link to = { `/books/delete/${book._id}`}>
-                      <MdOutlineAddBox className="text-2xl text-red-600"/> 
-                    </Link>
-                  </div>
-                </td>
-              </tr>
-            })}
-          </tbody>
-        </table>)}
+        :
+       <BooksTable books={books}/>}
       
     </div>
   )
